893a897764572628356b1d43bb5246905c3eb146
kuro-vale <jsalcedo218@gmail.com>
2022-05-24 17:59:45 -0500

    Update Templating.kt plugin

--- src/main/kotlin/com/kurovale/plugins/Templating.kt
+++ src/main/kotlin/com/kurovale/plugins/Templating.kt
@@ -1,11 +1,13 @@
 package com.kurovale.plugins
 
 import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
 import io.ktor.server.freemarker.*
 import io.ktor.server.application.*
 
 fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
 }
