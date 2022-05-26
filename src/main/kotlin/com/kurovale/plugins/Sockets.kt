8d6aa5762e82167aef5cf7128f1b6a08e618b72f
kuro-vale <jsalcedo218@gmail.com>
2022-05-26 16:24:05 -0500

    Add WebSocket echo server

--- src/main/kotlin/com/kurovale/plugins/Sockets.kt
+++ src/main/kotlin/com/kurovale/plugins/Sockets.kt
@@ -3,6 +3,8 @@ package com.kurovale.plugins
 import io.ktor.server.websocket.*
 import java.time.Duration
 import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.websocket.*
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -10,5 +12,16 @@ fun Application.configureSockets() {
         timeout = Duration.ofSeconds(15)
         maxFrameSize = Long.MAX_VALUE
         masking = false
+    }
+
+    routing {
+        webSocket("/general-english") {
+            send("You are connected")
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                send("You said: $receivedText")
+            }
+        }
     }
 }
